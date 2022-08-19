ab46ad89828c9e1c8ec78f856ba9602be378213c
nordo93 <orsone93@gmail.com>
2022-08-19 15:44:07 +0200

    w

--- tetrix.c
+++ tetrix.c
@@ -8,21 +8,21 @@
  * @copyright Copyright (c) 2022
  * 
  */
-/*! \mainpage My Personal Index Page
+/** @mainpage My Personal Index Page
  *
- * \section intro_sec Introduzione
+ * @section intro_sec Introduzione
  *
  * X-Tetris è una versione avanzata del gioco originale del Tetris. Per le regole del tetris puoi visitare la pagine
  * Wikipedia https://it.wikipedia.org/wiki/Tetris.
  *
- * \section Single Player
+ * @section Single Player
  * A differenza del Tetris originale, in X-Tetris il giocatore ha inizialmente a disposizione 20 (o più, a voi la
  * scelta) pezzi per ciascun tipo, detti tetramino, e una mossa consiste nello scegliere quale pezzo giocare, dove
  * farlo cadere e con quale rotazione.
  * Il campo di gioco è largo 10 e alto 15 posizioni. Una volta posizionato un tetramino, se una o più righe
  * orizzontali vengono riempite per intero, queste righe vengono cancellate come nel tetris originale.
  *
- * \subsection step1 Step 1: Opening the box
+ * @subsection step1 Step 1: Opening the box
  *
  * etc...
  */
